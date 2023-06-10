348cd0e2867616f40f6ab16a0d634df12c19e801
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-06-10 13:19:19 +0100

    make pricing page

--- src/pricing/components/Benefits.js
+++ src/pricing/components/Benefits.js
@@ -1,1 +1,36 @@
-export default function Benefits() {}
+const benefits = [
+    {
+        title: "One low price",
+        subtitle:"Save big. Get everything with a low monthly subscription.",
+    },
+    {
+        title: "No Limits",
+        subtitle:"Get complete access to everything on the site",
+    },
+    {
+        title: "Cancel anytime",
+        subtitle:"Pause or stop your subscription anytime you like",
+    },
+]
+
+export default function Benefits() {
+    return(
+        <div className="bg-black">
+            <div className="column-padding">
+                <div className="content-grid xl">
+                    {benefits.map((benefit)=>{
+                        return(
+                        <div key={benefit.title} className="spacing-base">
+                            <h3>
+                                {benefit.title}
+                                <br />
+                            </h3>
+                            <div>{benefit.subtitle}</div>
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pricing/components/Plans.js
+++ src/pricing/components/Plans.js
@@ -1,1 +1,28 @@
-export default function Plans() {}
+export default function Plans({plans}) {
+    const plan = plans[0]
+    return (
+        <div className="bg-salmon border-right">
+            <div className="column-padding center">
+                <div className="callout-wrap">
+                    <div className="plan">
+                        <div className="plan-wrap">
+                            <div className="plan-switch">
+                                Monthly
+                                <label className="switch">
+                                    <input type="checkbox"/>
+                                    <span className="slider" />
+                                </label>
+                                Yearly
+                            </div>
+                            <h2 className="plan-name">{plan.name}</h2>
+                            <div>Just $ {plan.price} / {plan.interval}</div>
+                            <div>
+                                <button></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
