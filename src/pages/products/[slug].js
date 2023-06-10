b8e56e2e8535c42f976b1cfaf4cf60385c7d68c2
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-06-10 10:00:15 +0100

    feat:added product page with dynamic rendering

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,3 +1,56 @@
-export default function ProductPage() {
-    
+import Image from "next/image";
+import PromoCard from "src/products/components/PromoCard";
+import { supabase } from "supabase";
+
+export default function ProductPage({ product }) {
+    return (
+        <section className="product-section">
+            <article className="product">
+                <div className="product-wrap">
+                    <Image width={1000} height={300} src={`/assets/${product.slug}.png`} alt={`${product.slug}`} />
+                </div>
+                <section>
+                    <header>
+                        <h3>{product.name}</h3>
+                    </header>
+                    <section>
+                        <div>
+                            <p>{product.description}</p>
+                        </div>
+                    </section>
+                </section>
+                <section>
+                    <PromoCard />
+                </section>
+            </article>
+        </section>
+    )
+}   
+
+export async function getStaticPaths() {
+    const { data: products } = await supabase.from("product").select("slug")
+
+    const paths = products.map(products => ({
+        params: {
+            slug: products.slug
+        }
+    }));
+
+    return {
+        paths:paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const slug = context.params.slug
+    let { data: product, error } = await supabase
+    .from('product')
+    .select("*")
+    // Filters
+    .eq('slug', slug).single()
+    return {
+        // Passed to the page component as props
+        props: { product }
+    }
 }
--- src/products/components/PromoCard.js
+++ src/products/components/PromoCard.js
@@ -1,1 +1,19 @@
-export default function PromoCard() {}
+import Link from "next/link";
+
+export default function PromoCard() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h4>Get Instant Access</h4>
+                    <p style={{ fontSize:"1rem"}}>
+                        Get access to this product plus many other when you subcribe.
+                    </p>
+                </div>
+            </div>
+            <Link href={'/pricing'} className="primary button">
+                Purchase
+            </Link>
+        </section>
+    )
+}
